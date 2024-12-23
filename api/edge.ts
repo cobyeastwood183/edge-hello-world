3783052bab58ab57229246ed0f127b3263e8bd6b
Coby Eastwood <cobyeastwood@ML6VVT2WLQ.local>
2024-12-23 10:59:14 -0800

    init

--- api/edge.ts
+++ api/edge.ts
@@ -1,5 +1,30 @@
+import Sentry from "../sentry.edge.config";
+
 export const config = {
   runtime: 'edge',
-}
+};
 
-export default (req) => new Response('Hello world!')
+export default async (req) => {
+  try {
+    const url = new URL(req.url);
+    const name = url.searchParams.get('name');
+
+    if (name) {
+      Sentry.captureException(new Error('Name parameter is not allowed!'));
+      return new Response(
+        JSON.stringify({ error: 'Name parameter is not allowed!' }),
+        { status: 400 }
+      );
+    }
+
+    return new Response(`Hello from ${process.env.VERCEL_REGION}`, {
+      status: 200,
+    });
+  } catch (err) {
+    Sentry.captureException(err);
+    return new Response(
+      JSON.stringify({ error: 'An unexpected error occurred.' }),
+      { status: 500 }
+    );
+  }
+};
